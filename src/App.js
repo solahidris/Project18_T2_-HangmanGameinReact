c5c9caf623009a2ab7cfcb61d748d4144ee77782
solahidris <emailsolah@gmail.com>
2023-07-11 23:39:07 +0800

    display game over message complet but messed up, redo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-
   // Random words array
   const wordArray = ["lion", "tiger", "wolf", "dinosaur"];
   // Map number of index of array
@@ -17,24 +16,71 @@ function App() {
     );
   };
 
-  // Submit Answer
-  const [AnswerSubmitted, setAnswerSubmitted] = useState("");
+  // Type Input Answer
+  const [answerTyped, setAnswerTyped] = useState("");
   const GuessWordSubmitHandler = (event) => {
-    const inputValue = event.target.value;
-    setAnswerSubmitted(inputValue);
-    AnswerSubmitted(""); // set value in input to empty again
+    const inputValue = event.target.value; // turn into const
+    setAnswerTyped(inputValue); // set answerTyped
+  };
+  // Submit Answer
+  const [answerSubmitted, setAnswerSubmitted] = useState("");
+  const submitAnswerHandler = () => {
+    setAnswerSubmitted(answerTyped); // Answer Submitted to Compare with Hangman
+    setAnswerTyped(""); // Clear Input Box
+    // compareAnswer();
   };
 
   // Attemts - Total, Left, Counter
-  const attemptsTotal = 3;
-  const [attemptsLeft, setAttemptsLeft] = useState(attemptsTotal);
   const attemptsHandler = () => {
     attemptsLeft !== 0 && setAttemptsLeft(attemptsLeft - 1);
   };
 
+  // Same answerSubmitted === wordArray[arrayIndex]
+  const attemptsTotal = 5;
+  const [attemptsLeft, setAttemptsLeft] = useState(attemptsTotal);
+  const [gameOver, setGameOver] = useState(false);
+  const [gameWin, setGameWin] = useState(false);
+  const [renderMessage, setRenderMessage] = useState(false);
+  // const compareAnswer = () => {
+  //   if (answerSubmitted !== wordArray[arrayIndex]) {
+  //     attemptsLeft > 0 && setAttemptsLeft(attemptsLeft - 1);
+  //     attemptsLeft === 1 && setGameOver(true); // if value != on last attempt, set gameover
+  //   } else if (answerSubmitted === wordArray[arrayIndex]) {
+  //     setGameWin(true)}
+  // };
+
+  // useEffect(() => {
+  //   if (gameWin) {
+  //     // Perform any additional actions you need here
+  //     // This code will execute when gameWin is set to true
+  //     // You can add any logic or API calls you require
+  //     setRenderMessage(true);
+  //   }
+  // }, [gameWin]);
+
+  const compareAnswer = () => {
+    if (answerSubmitted !== wordArray[arrayIndex]) {
+      attemptsLeft > 0 && setAttemptsLeft(attemptsLeft - 1);
+      attemptsLeft === 1 && setGameOver(true); // if value != on last attempt, set gameover
+    } else if (answerSubmitted === wordArray[arrayIndex]) {
+      setGameWin(true);
+    }
+  };
+
+  useEffect(() => {
+    if (gameWin) {setRenderMessage(true)}
+    compareAnswer();
+  }, [answerSubmitted, arrayIndex, attemptsLeft, gameWin]);
+
+
+  // RETURN
+  // RETURN
+  // RETURN
+  // RETURN
+  // RETURN
+  // RETURN
   return (
     <div className="bg-gradient-to-b from-stone-100 to-stone-500 min-h-[100vh]">
-
       {/* App Description */}
       <div className="pt-5 pb-2 flex flex-col text-center mx-[20%] mb-[2rem]">
         <h1 className="text-3xl font-bold">P18-Hangman</h1>
@@ -49,22 +95,24 @@ function App() {
       {/* Word to Guess */}
       <div className="flex flex-col justify-center">
         {/* Word to Guess - Text */}
-        <div className="text-center"><p>word to guess</p></div>
+        <div className="text-center">
+          <p>word to guess</p>
+        </div>
         {/* Word to Guess - Mapped */}
         <div className="flex justify-center gap-x-2">
-        {wordArray[arrayIndex].split("").map((item, index) => (
-          <label
-            key={index}
-            className="px-[1.2rem] py-[0.7rem] bg-stone-800 text-white rounded-lg"
-          >
-            {item}
-          </label>
-        ))}
+          {wordArray[arrayIndex].split("").map((item, index) => (
+            <label
+              key={index}
+              className="px-[1.2rem] py-[0.7rem] bg-stone-800 text-white rounded-lg"
+            >
+              {item}
+            </label>
+          ))}
         </div>
       </div>
 
       {/* New word Button */}
-      <div className="bg-sky-100/50 my-4 flex flex-col">
+      <div className="bg-sky-100/50 my-4 flex flex-col py-5">
         {/* Button - New Word */}
         <div className="flex justify-center">
           <button
@@ -76,16 +124,52 @@ function App() {
         </div>
         {/* Button - Text Below to show logic*/}
         <div>
-          <p className="text-xs">This button changes the arrayIndex Value to a random number</p>
+          <p className="text-xs">
+            This button changes the arrayIndex Value to a random number
+          </p>
           <p>Array Index Value: {arrayIndex}</p>
         </div>
       </div>
 
       {/* Input For Guessing */}
-      <div className="flex flex-col justify-center items-center bg-yellow-200/50">
-        <div><input type="text" placeholder="guess the word" className="bg-stone-100 p-3 rounded-lg"></input></div>
-        <div><button onClick={GuessWordSubmitHandler} className="bg-green-600 text-white p-3 rounded-lg mt-3">Submit Answer</button></div>
-        <p className="bg-red-400 p-10">fix the logic!!!</p>
+      <div className="flex flex-col justify-center items-center bg-yellow-200/50 py-5">
+        <div>
+          <input
+            type="text"
+            placeholder="guess the word"
+            value={answerTyped}
+            onChange={GuessWordSubmitHandler}
+            className="bg-stone-100 p-3 rounded-lg mb-3"
+          ></input>
+        </div>
+        <div>
+          <button
+            onClick={submitAnswerHandler}
+            className="bg-green-600 text-white p-3 rounded-lg"
+          >
+            Submit Answer
+          </button>
+        </div>
+
+        <p>answerTyped: {answerTyped}</p>
+        <p>answerSubmitted: {answerSubmitted}</p>
+      </div>
+
+      {/* Your Answer vs Guessing Word */}
+      <div className="bg-red-300/50 py-5">
+        <p className="text-center">Your Answer: {answerSubmitted}</p>
+        <p className="text-center">Question Answer: {wordArray[arrayIndex]} </p>
+        <p className="text-center">gameOver State: {gameOver.toString()} </p>
+        <p className="text-center">gameWin State: {gameWin.toString()} </p>
+        <p className="text-center">Question Answer: {wordArray[arrayIndex]} </p>
+        <p className="text-center">-</p>
+        {attemptsLeft >= 1 ? (
+          <p className="text-center">Tries Left: {attemptsLeft} / {attemptsTotal}</p>
+        ) : (
+          <p className="text-center">Game Over</p>
+        )}
+        {renderMessage && <p className="text-center">You Won!</p>}
+
       </div>
 
       {/* Attemps Remaining */}
